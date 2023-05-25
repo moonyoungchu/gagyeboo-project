e020ea7a6e3187aa5bb778353d2b10357be39301
Moonyoung Chu <cnansdud11@hanmail.net>
2023-05-26 01:16:55 +0900

    feat: 이번달지출 페이지 UI 작업

--- Frontend/gongyu-gagyebu/components/floating-button.tsx
+++ Frontend/gongyu-gagyebu/components/floating-button.tsx
@@ -1,21 +1,29 @@
 import Link from "next/link";
-import React from "react";
 
-interface FloatingButton {
-  children: React.ReactNode;
-  href: string;
-}
-
-export default function FloatingButton({ children, href }: FloatingButton) {
+export default function FloatingButton() {
   return (
-    <Link href={href}>
+    <Link href="/items/upload">
       <a
         className="fixed hover:bg-blue-500 border-0 aspect-square border-transparent 
       transition-colors cursor-pointer  bottom-10 right-5 
       shadow-xl bg-blue-400 rounded-full w-14 flex items-center 
       justify-center text-white "
       >
-        {children}
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
       </a>
     </Link>
   );
--- Frontend/gongyu-gagyebu/pages/index.tsx
+++ Frontend/gongyu-gagyebu/pages/index.tsx
@@ -2,39 +2,84 @@ import type { NextPage } from "next";
 import FloatingButton from "../components/floating-button";
 import Item from "../components/item";
 import Layout from "../components/layout";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <Layout title="공유가계부" hasTabBar>
-      <div className="flex flex-col divide-y">
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Item
-            id={i}
-            key={i}
-            title="신전떡볶이"
-            price={5000}
-            date={"03/05"}
-            hearts={1}
-          />
-        ))}
+    <Layout showMenubar={true}>
+      <Head>
+        <title>{`공유가계부`}</title>
+      </Head>
 
-        <FloatingButton href="/items/upload">
+      {/* 이번달 지출 페이지 */}
+
+      <div className="flex">2023년 5월</div>
+      <div className="flex">예산 100,000원</div>
+      <div className="flex">지출 100,000원</div>
+      <div className="flex">남은 금액 100,000원</div>
+
+      <div className="flex">그래프</div>
+
+      <div className="flex justify-end mr-1">
+      <div className="flex space-x-1 rounded-lg bg-slate-100 p-0.5">
+        <button
+          className="flex items-center rounded-md py-1 px-2 text-sm font-semibold bg-white shadow"
+          onClick={() => {
+            console.log(">>>btn2");
+          }}
+        >
           <svg
-            className="h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
           >
+            <path d="M5.25 12a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H6a.75.75 0 01-.75-.75V12zM6 13.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V14a.75.75 0 00-.75-.75H6zM7.25 12a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H8a.75.75 0 01-.75-.75V12zM8 13.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V14a.75.75 0 00-.75-.75H8zM9.25 10a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H10a.75.75 0 01-.75-.75V10zM10 11.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V12a.75.75 0 00-.75-.75H10zM9.25 14a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H10a.75.75 0 01-.75-.75V14zM12 9.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V10a.75.75 0 00-.75-.75H12zM11.25 12a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H12a.75.75 0 01-.75-.75V12zM12 13.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V14a.75.75 0 00-.75-.75H12zM13.25 10a.75.75 0 01.75-.75h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H14a.75.75 0 01-.75-.75V10zM14 11.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V12a.75.75 0 00-.75-.75H14z" />
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              fillRule="evenodd"
+              d="M5.75 2a.75.75 0 01.75.75V4h7V2.75a.75.75 0 011.5 0V4h.25A2.75 2.75 0 0118 6.75v8.5A2.75 2.75 0 0115.25 18H4.75A2.75 2.75 0 012 15.25v-8.5A2.75 2.75 0 014.75 4H5V2.75A.75.75 0 015.75 2zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75z"
+              clipRule="evenodd"
             />
           </svg>
-        </FloatingButton>
+
+          <span className=" text-slate-900 mx-1">날짜별</span>
+        </button>
+
+        <button
+          className="flex items-center rounded-md py-1 px-2 text-sm font-semibold"
+          onClick={() => {
+            console.log(">>>btn");
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.5 3A2.5 2.5 0 003 5.5v2.879a2.5 2.5 0 00.732 1.767l6.5 6.5a2.5 2.5 0 003.536 0l2.878-2.878a2.5 2.5 0 000-3.536l-6.5-6.5A2.5 2.5 0 008.38 3H5.5zM6 7a1 1 0 100-2 1 1 0 000 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <span className=" text-slate-600 mx-1">태그별</span>
+        </button>
+      </div>
+      </div>
+
+      <div className="flex flex-col">
+        {[1, 1, 1, 1, 1].map((_, i) => (
+          <Item id={i} key={i} title="신전떡볶이" price={5000} date={"03/05"} />
+        ))}
+
+
+        {[1, 1, 1].map((_, i) => (
+          <Item id={i} key={i} title="쇼핑(20%)" price={220000} date={""} />
+        ))}
+
+        <FloatingButton />
       </div>
     </Layout>
   );
