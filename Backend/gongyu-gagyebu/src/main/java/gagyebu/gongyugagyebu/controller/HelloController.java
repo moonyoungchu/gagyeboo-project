b6c8f70217e19e333029b6b7cd4fcc8f6ea7a5dd
parkdohyun <freesin94@gmail.com>
2023-05-29 15:21:04 +0900

    feat: template 형식 기본 base 추가

--- Backend/gongyu-gagyebu/src/main/java/gagyebu/gongyugagyebu/controller/HelloController.java
+++ Backend/gongyu-gagyebu/src/main/java/gagyebu/gongyugagyebu/controller/HelloController.java
@@ -3,6 +3,7 @@ package gagyebu.gongyugagyebu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,11 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello"; // template -> hello.html을 실행시켜라
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
