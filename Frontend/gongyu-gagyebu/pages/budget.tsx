1dbfd253be1c4530e03376143433e69df99c229d
Moonyoung Chu <cnansdud11@hanmail.net>
2023-05-31 23:03:07 +0900

    feat: 예산수정,아이템추가,아이템수정페이지 추가 및 UI 레이아웃 수정
    - 날짜별, 태그별 버튼 기능 추가
    - 연간소비통계 페이지 레이아웃 작성

---
+++ Frontend/gongyu-gagyebu/pages/budget.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Budget: NextPage = () => {
+  return (
+    <div className="text-xl"> 예산수정 페이지</div>
+  );
+};
+
+export default Budget;
---
+++ Frontend/gongyu-gagyebu/pages/items/add.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Add: NextPage = () => {
+  return (
+    <div>추가 페이지</div>
+  );
+};
+
+export default Add;
