c84ce3c2635020ef10d25483915375e7f542deb3
parkdohyun <freesin94@gmail.com>
2023-06-20 21:14:35 +0900

    test

---
+++ Backend/accountbookdatabase/src/main/java/com/packt/accountbookdatabase/AccountbookdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.packt.accountbookdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountbookdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountbookdatabaseApplication.class, args);
+	}
+
+}
